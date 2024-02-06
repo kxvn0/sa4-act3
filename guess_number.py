d10ecd74d69450ec805df4e07bf7b214cb3091e0
Kevin Cosme <official.kevo12@gmail.Cosme>
2024-02-06 01:42:51 -0600

    Changed the original code for the program in order to ask the user to enter multiple guesses until they guess the correct number.

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,13 @@
 number = 10
 
 print('I am thinking of a number....')
-guess = int(input('What number am I thinking of? '))
+while True:
 
-if guess == number:
-    print('Congragulations! You guessed the right number.')
-else:
-    print(f'Sorry! The number was {number}.')
+    guess = (int(input('What number am I thinking of?')))
+    if guess == number:
+        print('Congratulations! You guessed the right number.')
+        break
+    else:
+        print('Sorry! That is not the right number. Try again: ')
 
-    
+
