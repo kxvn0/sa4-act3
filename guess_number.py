057b390e8ea04b5b8271852e536a742026b46bc9
Kevin Cosme <official.kevo12@gmail.Cosme>
2024-02-06 02:09:08 -0600

    Added a decrement so that user has a limited number of guesses to guess the correct number.

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,20 @@
 number = 10
+num_of_guess = 4
 
-print('I am thinking of a number....')
-while True:
 
-    guess = (int(input('What number am I thinking of?')))
+while num_of_guess > 0:
+    guess = int(input('What number am I thinking of?'))
+
     if guess == number:
         print('Congratulations! You guessed the right number.')
         break
-    else:
-        print('Sorry! That is not the right number. Try again: ')
+    elif guess < number:
+        print('Sorry! The number you entered is too low. Try again: ')
+    elif guess > number:
+        print('Sorry! The number you entered is too high. Try again: ')
+    num_of_guess -= 1
 
+    print(f'You have {num_of_guess} guesses left.')
 
+if num_of_guess ==0:
+    print(f'Sorry! You have run out of guesses. The number was {number}.')
