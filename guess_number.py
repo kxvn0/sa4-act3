a1557e9f63f4dbb17f6526192da6f82180483048
Kevin Cosme <official.kevo12@gmail.Cosme>
2024-02-06 01:25:06 -0600

    Created a program in which the user a number that is hardcoded in the program.

---
+++ guess_number.py
@@ -0,0 +1,11 @@
+number = 10
+
+print('I am thinking of a number....')
+guess = int(input('What number am I thinking of? '))
+
+if guess == number:
+    print('Congragulations! You guessed the right number.')
+else:
+    print(f'Sorry! The number was {number}.')
+
+    
